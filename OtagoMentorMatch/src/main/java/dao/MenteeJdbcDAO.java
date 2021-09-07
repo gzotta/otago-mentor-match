9b0ea6c37ef3359d2cc8e8a48b1ae0984fe11dd8
gzotta <zottamendez@hotmail.com>
2021-09-07 16:15:01 +1200

    getMenteeByFname changed to getMenteeByEmail #23
    Also validateCredentials method updated to use getMenteeByEmail instead
    of getMenteeByFname.

--- OtagoMentorMatch/src/main/java/dao/MenteeJdbcDAO.java
+++ OtagoMentorMatch/src/main/java/dao/MenteeJdbcDAO.java
@@ -72,10 +72,10 @@ public class MenteeJdbcDAO {
         }//end of method to save Mentee
 
 
-    // method to get Mentee by first name.
+    // method to get Mentee by email.
     // support method only - used by validateCredentials() below.
-    public Mentee getMenteeByFname(String fname) {
-        String sql = "SELECT * FROM mentee WHERE fname = ?";
+    public Mentee getMenteeByEmail(String email) {
+        String sql = "SELECT * FROM mentee WHERE email = ?";
 
         try (
             // get a connection to the database.
@@ -83,7 +83,7 @@ public class MenteeJdbcDAO {
             // create the statement.
             PreparedStatement stmt = dbCon.prepareStatement(sql);) {
                 // copy the data from the Mentee domain object into the SQL parameters.
-                stmt.setString(1, fname);
+                stmt.setString(1, email);
                 // execute the query.
                 ResultSet rs = stmt.executeQuery();
             
@@ -91,9 +91,9 @@ public class MenteeJdbcDAO {
                     // get the data out of the query.
                     int menteeId = rs.getInt("mentee_id");
                     String password = rs.getString("password");
+                    String fname = rs.getString("fname");
                     String lname = rs.getString("lname");
                     String phone_number = rs.getString("phone_number");
-                    String email  = rs.getString("email");
                     String year_of_study = rs.getString("year_of_study");
                     String motivation_for_joining_omm = rs.getString("motivation_for_joining_omm");
                     String industry_of_interest = rs.getString("industry_of_interest");
@@ -107,9 +107,9 @@ public class MenteeJdbcDAO {
                     Mentee mentee = new Mentee();
                     mentee.setMenteeId(menteeId);
                     mentee.setMenteePassword(password);
+                    mentee.setFname(fname);
                     mentee.setLname(lname);
                     mentee.setPhoneNumber(phone_number);
-                    mentee.setEmail(email);
                     mentee.setYearOfStudy(year_of_study);
                     mentee.setMotivationForJoiningOMM(motivation_for_joining_omm);
                     mentee.setIndustryOfInterest(industry_of_interest);
@@ -131,9 +131,9 @@ public class MenteeJdbcDAO {
         
 
     // method to sign users in.
-    // accesses getMenteeByFname() above.
-    public Boolean validateCredentials(String fname, String password) {
-        Mentee mentee = getMenteeByFname(fname);
+    // accesses getMenteeByemail() above.
+    public Boolean validateCredentials(String email, String password) {
+        Mentee mentee = getMenteeByEmail(email);
         if ((mentee != null) && (mentee.getMenteePassword().equals(password))) {
             return true;
         } else {
@@ -145,7 +145,7 @@ public class MenteeJdbcDAO {
     
     // method to delete Mentee.
     public void removeMentee(Mentee mentee) {
-        String sql = "DELETE FROM mentee WHERE fname = ?";
+        String sql = "DELETE FROM mentee WHERE email = ?";
         
         try (
             // get connection to database.
@@ -153,7 +153,7 @@ public class MenteeJdbcDAO {
             // create the statement.
             PreparedStatement stmt = dbCon.prepareStatement(sql);) {
                 // copy the data from the Mentee domain object into the SQL parameters.
-                stmt.setString(1, mentee.getFname());
+                stmt.setString(1, mentee.getEmail());
                 stmt.executeUpdate(); // execute the statement
             
             } catch (SQLException ex) {  // we are forced to catch SQLException
