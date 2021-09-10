9eb6c4f0f4d893f6e169aa0069401080f4e905e4
jamespettitt <pettitt.james1@gmail.com>
2021-09-10 16:09:52 +1200

    to camelcase

--- OtagoMentorMatch/src/main/java/domain/JournalEntry.java
+++ OtagoMentorMatch/src/main/java/domain/JournalEntry.java
@@ -1,9 +1,9 @@
 package domain;
 
 public class JournalEntry {
-    private Integer journal_entry_id;
-    private String topics_covered;
-    private String length_of_session;
+    private Integer journalEntryId;
+    private String topicsCovered;
+    private String lengthOfSession;
     private String notes;
     private Integer matchId;
 
@@ -14,38 +14,38 @@ public class JournalEntry {
 
 
     //constructor with all fields.
-    public JournalEntry(Integer journal_entry_id, String topics_covered, String length_of_session, String notes, Integer matchId) {
-        this.journal_entry_id = journal_entry_id;
-        this.topics_covered = topics_covered;
-        this.length_of_session = length_of_session;
+    public JournalEntry(Integer journalEntryId, String topicsCovered, String lengthOfSession, String notes, Integer matchId) {
+        this.journalEntryId = journalEntryId;
+        this.topicsCovered = topicsCovered;
+        this.lengthOfSession = lengthOfSession;
         this.notes = notes;
         this.matchId = matchId;
     }
 
 
     //getters and setters.
-    public Integer getJournal_entry_id() {
-        return this.journal_entry_id;
+    public Integer getJournalEntryId() {
+        return this.journalEntryId;
     }
 
-    public void setJournal_entry_id(Integer journal_entry_id) {
-        this.journal_entry_id = journal_entry_id;
+    public void setJournalEntryId(Integer journalEntryId) {
+        this.journalEntryId = journalEntryId;
     }
 
-    public String getTopics_covered() {
-        return this.topics_covered;
+    public String getTopicsCovered() {
+        return this.topicsCovered;
     }
 
-    public void setTopics_covered(String topics_covered) {
-        this.topics_covered = topics_covered;
+    public void settopicsCovered(String topicsCovered) {
+        this.topicsCovered = topicsCovered;
     }
 
-    public String getLength_of_session() {
-        return this.length_of_session;
+    public String getLengthOfSession() {
+        return this.lengthOfSession;
     }
 
-    public void setLength_of_session(String length_of_session) {
-        this.length_of_session = length_of_session;
+    public void setLengthOfSession(String lengthOfSession) {
+        this.lengthOfSession = lengthOfSession;
     }
 
     public String getNotes() {
@@ -70,9 +70,9 @@ public class JournalEntry {
     @Override
     public String toString() {
         return "{" +
-            " journal_entry_id='" + journal_entry_id + "'" +
-            ", topics_covered='" + topics_covered + "'" +
-            ", length_of_session='" + length_of_session + "'" +
+            " journal_entry_id='" + journalEntryId + "'" +
+            ", topicsCovered='" + topicsCovered + "'" +
+            ", lengthOfSession='" + lengthOfSession + "'" +
             ", notes='" + notes + "'" +
             ", matchId='" + matchId + "'" +
             "}";
