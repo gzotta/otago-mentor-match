b2047a95e2301695a859a49c8869a9be9686c802
gzotta <zottamendez@hotmail.com>
2021-09-15 15:47:09 +1200

    MenteeModule initialized. #48 Save and delete mentee methods added. More methods will be added.

---
+++ OtagoMentorMatch/src/main/java/web/MenteeModule.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web;
+
+import dao.MenteeJdbcDAO;
+import domain.Mentee;
+import org.jooby.Jooby;
+import org.jooby.Result;
+import org.jooby.Status;
+
+
+/**
+ *
+ * @author zotta
+ */
+public class MenteeModule extends Jooby {
+
+    public MenteeModule (MenteeJdbcDAO menteeDao){
+        
+        // Save (POST) a Mentee.
+        post("/api/signUpMentee", (req, rsp) -> {
+            Mentee mentee = req.body().to(Mentee.class);
+            menteeDao.saveMentee(mentee);
+            rsp.status(Status.CREATED);
+        });
+
+
+        // DELETE a Mentee.
+        delete("/api/mentees/:email", (req, rsp) -> {
+            String email = req.param("email").value();
+            Mentee mentee = menteeDao.getMenteeByEmail(String.valueOf(email));
+            menteeDao.removeMentee(mentee);
+            rsp.status(Status.NO_CONTENT);
+        });
+
+
+
+    }
+    
+    
+
+    
+}
