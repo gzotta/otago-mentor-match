5cbba3d4881548e61017b76756f8dde050672110
jamespettitt <pettitt.james1@gmail.com>
2021-09-16 16:32:23 +1200

    post delete

---
+++ OtagoMentorMatch/src/main/java/web/MenteeFeedbackFormModule.java
@@ -0,0 +1,33 @@
+package web;
+
+import dao.MenteeFeedbackFormJdbcDAO;
+import domain.MenteeFeedbackForm;
+import org.jooby.Jooby;
+import org.jooby.Result;
+import org.jooby.Status;
+
+/**
+ *
+ * @author James Pettitt
+ */
+public class MenteeFeedbackFormModule extends Jooby {
+    
+    public MenteeFeedbackFormModule(MenteeFeedbackFormJdbcDAO menteeFeedbackFormDao){
+        
+        // Save (POST) a MenteeFeedbackForm.
+        post("/api/menteeFeedbackForms", (req, rsp) -> {
+            MenteeFeedbackForm menteeFeedbackForm = req.body().to(MenteeFeedbackForm.class);
+            menteeFeedbackFormDao.saveMenteeFeedbackForm(menteeFeedbackForm);
+            rsp.status(Status.CREATED);
+        });
+    
+        // DELETE a MenteeFeedbackForm.
+        delete("/api/menteeFeedbackForms/:id", (req, rsp) -> {
+            String id = req.param("id").value(); 
+            MenteeFeedbackForm menteeFeedbackForm = menteeFeedbackFormDao.getFormById(Integer.parseInt(id));
+            menteeFeedbackFormDao.removeMenteeFeedbackForm(menteeFeedbackForm);
+            rsp.status(Status.NO_CONTENT);
+        });
+    }
+
+}
