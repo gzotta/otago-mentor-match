9101bca28d9df566d0ae3c36d4e42d66b5b7fe4b
gzotta <zottamendez@hotmail.com>
2021-09-19 15:33:59 +1200

    Missing  brackets added. #48

--- OtagoMentorMatch/src/main/java/web/MenteeModule.java
+++ OtagoMentorMatch/src/main/java/web/MenteeModule.java
@@ -6,22 +6,21 @@ import org.jooby.Jooby;
 import org.jooby.Result;
 import org.jooby.Status;
 
-
 /**
  *
  * @author zotta
  */
 public class MenteeModule extends Jooby {
 
-    public MenteeModule (MenteeJdbcDAO menteeDao){
-        
+    public MenteeModule(MenteeJdbcDAO menteeDao) {
+
         // Save (POST) a Mentee.
         post("/api/mentees", (req, rsp) -> {
             Mentee mentee = req.body().to(Mentee.class);
             menteeDao.saveMentee(mentee);
             rsp.status(Status.CREATED);
- 
 
+        });
 
         // DELETE a Mentee.
         delete("/api/mentees/:email", (req, rsp) -> {
@@ -31,11 +30,6 @@ public class MenteeModule extends Jooby {
             rsp.status(Status.NO_CONTENT);
         });
 
-
-
     }
-    
-    
 
-    
 }
