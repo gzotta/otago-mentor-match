c86c44d681a9d6bc1da9f008f6fed5cf08992ce5
gzotta <zottamendez@hotmail.com>
2021-09-22 16:47:12 +1200

    Mentor factories and controller initialised. #66

--- OtagoMentorMatch/public/js/match.js
+++ OtagoMentorMatch/public/js/match.js
@@ -1,1 +1,22 @@
 "use strict";
+
+/*
+ * AngularJS module for the match app, and other plugglable modules.
+ * ngResource = REST resource, ngStorage = local storage module.
+ */
+var module = angular.module("MatchingApp", ["ngResource", "ngStorage"]);
+
+///////Mentor Factories and Controller///////
+
+// Factory for the ngResource object that will get the all the Mentors from the web service.
+module.factory("mentorAPI", function($resource) {
+    return $resource("/api/mentors");
+});
+
+// Factory for the ngResource object that will get a Mentor by email from the web service.
+module.factory("mentorsAPI", function($resource) {
+    return $resource("/api/mentors/:email");
+});
+
+// Controller for managing Mentor resources.
+module.controller("MentorController", function(mentorAPI, mentorsAPI) {});
