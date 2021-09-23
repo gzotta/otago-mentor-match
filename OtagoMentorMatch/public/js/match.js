f772c0ace3f5fe556f914504ff7198964003b929
Taine Bayly <bayta257@student.otago.ac.nz>
2021-09-23 22:22:15 +1200

    mentee controller and sessions storage first test

--- OtagoMentorMatch/public/js/match.js
+++ OtagoMentorMatch/public/js/match.js
@@ -18,6 +18,16 @@ module.factory("mentorsAPI", function($resource) {
     return $resource("/api/mentors/:email");
 });
 
+// factory for th ngResource object that will get the all the Mentees from the web service.
+module.factory("menteeAPI", function($resource){
+    return $resource("/api/mentees");
+});
+
+//factor for the ngResource object that will get all the mentees by email from the web service.
+module.factory("menteesAPI", function($resource){
+    return $resource("/api/mentees/:email");
+});
+
 // Controller for managing Mentor resources.
 module.controller(
     "MentorController",
@@ -27,6 +37,19 @@ module.controller(
         this.registerMentor = function(mentor) {
             alert("Register Mentor");
             console.log(mentor);
+        };
+    }
+);
+
+// Controller for managing Mentee resources.
+module.controller(
+    "MenteeController",
+    function(menteeAPI, menteesAPI, $window) {
+        //alert("in controller");
+
+        this.registerMentee = function(mentee) {
+            alert("Register Mentee");
+            console.log(mentee);
         };
     }
 );
