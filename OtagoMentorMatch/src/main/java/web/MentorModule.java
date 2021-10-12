17e772b84c22ba7c885667f4f7e8d3d01edc909b
jamespettitt <pettitt.james1@gmail.com>
2021-10-12 21:44:50 +1300

    setEmail and  new delete mentor api call

--- OtagoMentorMatch/src/main/java/web/MentorModule.java
+++ OtagoMentorMatch/src/main/java/web/MentorModule.java
@@ -27,7 +27,7 @@ public class MentorModule extends Jooby {
         });
 
         // DELETE a Mentor.
-        delete("/api/mentors/:email", (req, rsp) -> {
+        delete("/api/mentors/mentor/:email", (req, rsp) -> {
             String email = req.param("email").value();
             Mentor mentor = mentorDao.getMentorByEmail(String.valueOf(email));
             mentorDao.removeMentor(mentor);
